46f717b8abb1ef948fea5fe64fbf65e08138570f
Liza Bragina <catherineby123@yandex.ru>
2025-03-01 22:10:00 +0300

    Первый коммит

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,4 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
-import path from 'path';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -9,22 +8,6 @@ const config: StorybookConfig = {
     '@storybook/addon-onboarding',
     '@storybook/addon-interactions'
   ],
-  webpackFinal: async (config) => {
-    config.resolve
-      ? (config.resolve.alias = {
-          ...config.resolve.alias,
-          '@pages': path.resolve(__dirname, '../src/pages'),
-          '@components': path.resolve(__dirname, '../src/components'),
-          '@ui': path.resolve(__dirname, '../src/components/ui'),
-          '@ui-pages': path.resolve(__dirname, '../src/components/ui/pages'),
-          '@utils-types': path.resolve(__dirname, '../src/utils/types'),
-          '@api': path.resolve(__dirname, '../src/utils/burger-api.ts'),
-          '@slices': path.resolve(__dirname, '../src/services/slices'),
-          '@selectors': path.resolve(__dirname, '../src/services/selectors')
-        })
-      : null;
-    return config;
-  },
   framework: {
     name: '@storybook/react-webpack5',
     options: {
